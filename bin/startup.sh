c76cf1e082645f4060ca15befd0690720040ad14
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-08-23 20:51:01 -0300

    update startup script

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,7 +3,9 @@ cd /home/pi/generate_display_html
 eval 'ssh-agent' >> ~/Desktop/log.txt
 ssh-add bin/github/intus_github >> ~/Desktop/log.txt
 sleep 5
+echo 'waiting 5s' >> ~/Desktop/log.txt
 git pull >> ~/Desktop/log.txt
+echo 'waiting 5s again' >> ~/Desktop/log.txt
 sleep 5
 source venv/bin/activate
 cd generate_display_html
