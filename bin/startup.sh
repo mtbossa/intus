6ade0ca9648fa9696a1a1f0a71591c019020b051
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-08-23 20:21:38 -0300

    Update startup script

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-cd /home/pi/generate_display_html || exit
-eval 'ssh-agent' >> log.txt
-ssh-add bin/github/intus_github >> log.txt
-git pull >> log.txt
-source venv/bin/activate >> log.txt
-cd generate_display_html || exit >> log.txt
-python3.9 main.py >> log.txt
-unclutter -idle 0 >> log.txt
+cd /home/pi/generate_display_html
+eval 'ssh-agent' >> ~/Desktop/log.txt
+ssh-add bin/github/intus_github >> ~/Desktop/log.txt
+git pull >> ~/Desktop/log.txt
+source venv/bin/activate
+cd generate_display_html
+python3.9 main.py
+unclutter -idle 0
