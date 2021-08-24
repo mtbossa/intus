88bf8aa876c4de9476d868bd7ff87ade3c5e14aa
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-08-24 16:56:31 -0300

    Updated scripts

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 cd /home/pi/generate_display_html || exit
+chmod +x bin/startup.sh
 git remote set-url origin git@github.com:mtbossa/generate_display_html.git
 chmod 400 bin/github/intus_github
 mkdir -p /home/pi/.config/autostart && cp bin/autostart-display.desktop /home/pi/.config/autostart/autostart-display.desktop
